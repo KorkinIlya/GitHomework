0912c7bb4681b19479388bbde5efd659d31fc87d
Korkin Ilya <iokorkin@edu.hse.ru>
2024-09-22 12:39:39 +0300

    Added changes to to make 11th task

--- nanoproject.py
+++ nanoproject.py
@@ -1,7 +1,5 @@
 print("Hello world!")
 print("Enter two integers")
 a, b = map(int, input().split())
-<<<<<<< HEAD
 #I resolved the conflict
->>>>>>> KorkinIOwithConflicts
 print("Result of substruction:", a-b)
